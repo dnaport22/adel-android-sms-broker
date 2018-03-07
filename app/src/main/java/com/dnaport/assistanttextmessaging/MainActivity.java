dc6a24693830fbb7e4d98187cf9b9e571a8ee124
Navdeep Dhuti <navdeepdhuti@AT-MacPro-005.local>
2018-03-07 16:57:14 +0000

    Design updates

--- app/src/main/java/com/dnaport/assistanttextmessaging/MainActivity.java
+++ app/src/main/java/com/dnaport/assistanttextmessaging/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AssistTrigger = getLatestTrigger();
+//        AssistTrigger = getLatestTrigger();
         AssistantConfig.setTrigger(AssistTrigger);
 
         mButtonSaveConfig = (Button) findViewById(R.id.saveConfigButton);
